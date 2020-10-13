cf55a534aa774dd9a6a044da14f821e4e9f5187a
saniag081 <sgonzalezhernandez081@gmail.com>
2020-10-13 18:08:13 -0500

    add functionality input

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,26 @@
-async function getPokemon(id) {
-	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-	const data = await response.json()
-	return data;
-}
+(function () {
+	const search = document.querySelector('#search');
 
-async function init() {
-	const firstPokemon = await getPokemon(150)
-	window.pokemonName.textContent = firstPokemon.name;
-	window.pokemonImage.setAttribute('src', firstPokemon.sprites.front_default)
-}
-init()
+	async function getPokemon(id) {
+		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+		const data = await response.json();
+		return data;
+	}
+
+	async function init() {
+		const firstPokemon = await getPokemon(150);
+		updatePokemon(firstPokemon);
+	}
+
+	function updatePokemon(pokemon) {
+		window.pokemonName.textContent = pokemon.name;
+		window.pokemonImage.setAttribute('src', pokemon.sprites.front_default)
+	}
+
+	search.addEventListener('change', async () => {
+		const pokemon = await getPokemon(search.value);
+		updatePokemon(pokemon)
+		// console.log(search.value);
+	})
+	init()
+})();
